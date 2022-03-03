25bbc706d2fb73ff609e8e26d45c38e43a17c541
Weylland <samiernicolas62@gmail.com>
2022-03-03 16:11:37 +0100

    popup
    J'ai ajouté les popups

--- include/nav.php
+++ include/nav.php
@@ -1,9 +1,12 @@
+<div id="cassetteContainer">
+    <img src="./asset/img/rouage.png" alt="" id="rouageGauche">
+    <img src="./asset/img/rouage.png" alt="" id="rouageDroite">
+    <img src="./asset/img/cassette-popup.png" alt="" id="cassette">
+    <p id="cassetteP">
+        Acceuil
+    </p>
+</div>
 <nav>
-    <div id="container">
-        <img src="./asset/img/rouage.png" alt="" id="rouageGauche">
-        <img src="./asset/img/rouage.png" alt="" id="rouageDroite">
-        <img src="./asset/img/cassette-popup.png" alt="" id="cassette">
-    </div>
     <div id="navBtnCon">
         <div class="navBtn">
             <a href="./index.php"><i class="fa-solid fa-play"></i></a>
@@ -21,7 +24,7 @@
             <a href="./exposition3.php"><i class="fa-solid fa-forward"></i></a>
         </div>
         <div class="navBtn">
-            <a href="./contact"><i class="fa-solid fa-stop"></i></a>
+            <a href="./contact.php"><i class="fa-solid fa-stop"></i></a>
         </div>
     </div>
 
@@ -37,6 +40,5 @@
         <div class="navBtnShadow">
         </div>
     </div>
-
     <audio src="./asset/audio/button.mp3" id="buttonSound" autoplay></audio>
 </nav>
--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
+// Nav
 const navBtnCon = document.getElementById("navBtnCon");
 const navBtns = navBtnCon.getElementsByClassName("navBtn");
+// POPUP
+const cassetteContainer = document.getElementById("cassetteContainer");
+const cassetteP = document.getElementById("cassetteP");
 
 if (window.location.href === "http://localhost:8000/index.php") {
   navBtns[0].classList.add("active");
@@ -11,4 +15,29 @@ if (window.location.href === "http://localhost:8000/index.php") {
   navBtns[3].classList.add("active");
 } else if (window.location.href === "http://localhost:8000/contact.php") {
   navBtns[4].classList.add("active");
+}
+
+for (let i = 0; i < navBtns.length; i++) {
+  let navBtnsPos = navBtns[i].getBoundingClientRect();
+  navBtns[i].addEventListener("mouseenter", () => {
+    setTimeout(() => {
+      cassetteContainer.style.left = navBtnsPos.x - 28 + "px";
+    }, 500);
+    cassetteContainer.style.top = "100px";
+    cassetteContainer.style.transition = "0.5s ease-in-out";
+    if (i == 0) {
+      cassetteP.innerHTML = "Acceuil";
+    } else if (i == 1) {
+      cassetteP.innerHTML = "expo 1";
+    } else if (i == 2) {
+      cassetteP.innerHTML = "expo 2";
+    } else if (i == 3) {
+      cassetteP.innerHTML = "expo 3";
+    } else if (i == 4) {
+      cassetteP.innerHTML = "Contact";
+    }
+  });
+  navBtns[i].addEventListener("mouseleave", () => {
+    cassetteContainer.style.top = "-250px";
+  });
 }
